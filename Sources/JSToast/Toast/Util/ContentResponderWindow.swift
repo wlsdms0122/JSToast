c5e4e06824bc0ed1a62e8a8ecab2a23c378fb394
Jineun Jeong <wlsdms0122@gmail.com>
2024-10-14 22:23:51 +0900

    fix(): fix an issue where the `hitTest(_:with:)` of `ContentResponderWindow` behaves unexpectedly on iOS 18+

--- Sources/JSToast/Toast/Util/ContentResponderWindow.swift
+++ Sources/JSToast/Toast/Util/ContentResponderWindow.swift
@@ -1,8 +1,8 @@
 //
 //  ContentResponderWindow.swift
-//  
 //
-//  Created by JSilver on 2023/02/11.
+//
+//  Created by jsilver on 2023/02/11.
 //
 
 import UIKit
@@ -39,12 +39,32 @@ class ContentResponderWindow: UIWindow {
     
     // MARK: - Lifecycle
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        let view = super.hitTest(point, with: event)
-        guard view != rootViewController?.view else { return nil }
-        return view
+        if #available(iOS 18, *) {
+            let view = super.hitTest(point, with: event)
+            guard let view, _hitTest(point, from: view) != rootViewController?.view else { return nil }
+            return view
+        } else {
+            let view = super.hitTest(point, with: event)
+            guard view != rootViewController?.view else { return nil }
+            return view
+        }
     }
     
     // MARK: - Public
     
     // MARK: - Private
+    private func _hitTest(_ point: CGPoint, from view: UIView) -> UIView? {
+        let converted = convert(point, to: view)
+        
+        guard view.bounds.contains(converted)
+            && view.isUserInteractionEnabled
+            && !view.isHidden
+            && view.alpha > 0
+        else { return nil }
+        
+        return view.subviews.reversed()
+            .reduce(Optional<UIView>.none) { result, view in
+                result ?? _hitTest(point, from: view)
+            } ?? view
+    }
 }
